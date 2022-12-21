5acc566032f678ea58c8c6b3957113ade7c8dbea
robelsiam <robelesiam@gmail.com>
2022-12-21 03:02:05 -0500

    Project card update
    making the project cards look pretty

--- src/components/About.js
+++ src/components/About.js
@@ -5,14 +5,7 @@ const About = () => {
         <section className='about'>
             <h2>About Me</h2>
             <p>
-                Lorem ipsum dolor sit amet consectetur,
-                adipisicing elit. Neque, pariatur odio.
-                Maxime, labore nihil illo ea voluptate suscipit aliquid animi
-                possimus laudantium fugit obcaecati harum?
-                Assumenda maiores nam magnam animi? Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Eaque voluptatibus vitae, rem corrupti id consectetur!
-                Sapiente laboriosam nam excepturi exercitationem natus unde, harum
-                consequatur vero amet consectetur quod sunt autem!
+                I am 21 and currently studying for my BS in Computer Science at GMU.
             </p>
         </section>
     );
--- src/components/Social.js
+++ src/components/Social.js
@@ -4,8 +4,8 @@ const Social = () => {
             <h2>Socials</h2>
 
             <a href="mailto:robelesiam@gmail.com" className="button">robelesiam@email.com</a>
-            <a href="https://www.linkedin.com/in/robel-siam-224291222/" target="_blank" className="button">LinkedIn</a>
-            <a href="https://github.com/robelsiam" target="_blank" className="button">GitHub</a>
+            <a href="https://www.linkedin.com/in/robel-siam-224291222/" target="_blank" rel="noreferrer" className="button">LinkedIn</a>
+            <a href="https://github.com/robelsiam" target="_blank" rel="noreferrer" className="button">GitHub</a>
         </div>
     );
 }
