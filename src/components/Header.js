802ab2f3cfd090463e60c7cdb348848fa426f913
robelsiam <robelesiam@gmail.com>
2022-12-29 14:53:25 -0500

    updated button to resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,8 @@
+import { Document } from 'react-pdf';
 // Import Assets
 import morpheus from '../assets/morpheus.gif';
+import resume from '../assets/RobelSiamResume4website.pdf';
+
 
 const Header = () => {
     return (
@@ -11,7 +14,7 @@ const Header = () => {
                 <h1>Hello, my name is Robel Siam welcome to my website</h1>
             </div>
             <div className="header__content">
-                <a href="mailto:robelesiam@gmail.com" rel="noreferrer" className='button'>Check out my resume</a>
+                <a href={resume} target="_blank" rel="noreferrer" className='button'>Check out my resume</a>
 
                 <a href="mailto:robelesiam@gmail.com" target="_blank" rel="noreferrer" className='button'>Send me an email</a>
             </div>
