a22d571bf13cd95d28391f225a9811b0d1cc7316
robelsiam <86976489+robelsiam@users.noreply.github.com>
2023-03-01 14:04:52 -0500

    Update Social.js

--- src/components/Social.js
+++ src/components/Social.js
@@ -6,7 +6,7 @@ const Social = () => {
 
             <a href="https://www.linkedin.com/in/robel-siam-224291222/" target="_blank" rel="noreferrer" className="button">LinkedIn</a>
             <a href="https://github.com/robelsiam" target="_blank" rel="noreferrer" className="button">GitHub</a>
-            <a href="https://letterboxd.com/GCM703/films" target="_blank" rel="noreferrer"className="button">Letterboxd</a>
+            <a href="https://letterboxd.com/gcm703/films/by/entry-rating/" target="_blank" rel="noreferrer"className="button">Letterboxd</a>
         </div>
         
         
