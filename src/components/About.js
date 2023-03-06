1571e188d0d3d929cac17792476fe8705994d7b1
robelsiam <86976489+robelsiam@users.noreply.github.com>
2023-03-06 09:27:01 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
         <section className='about'>
             <h2>Bio</h2>
             <p>
-            I am 21 and currently studying for my BS in Computer Science at GMU. Computer programs and the quality they have to 
+            Computer programs and the quality they have to 
             present and transfer any form of information the author is capable of conceiving has always been a topic of interest 
             for me. From medical equipment to online banking, computer software is turning into a staple of all aspects of life 
             which is why I spend so much time learning about the craft.
